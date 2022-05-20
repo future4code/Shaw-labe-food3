b9344774421219e90151b5bbe67f021287082b7e
yasminconc <yasminsilvaconc@gmail.com>
2022-05-20 16:26:47 -0300

    loading

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import React from 'react';
 import Router from "./routes/Router";
 import { GlobalStyle } from "./AppStyle"
 import GlobalState from './global/GlobalState';
-
+import Carregar from './pages/TelaDeCarregamento/Carregar';
 
 function App() {
     return (
         <div>
             <GlobalStyle />
             <GlobalState>
-                <Router />
+            <Router />
             </GlobalState>
+        
+            
         </div>
     );
 }
